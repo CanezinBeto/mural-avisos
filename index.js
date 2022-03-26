5ef658ef9e50893e0c492cbdd6a1929a9dbcfa8d
CanezinBeto <albertocanezin@gmail.com>
2022-03-26 19:16:00 -0300

    Criando API para POST

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
-const posts = [
-  { id: '1', title: 'Teste Mural', description: 'Descrição Tste' },
-  { id: '1', title: 'Teste Mural', description: 'Descrição Tste' },
-  { id: '1', title: 'Teste Mural', description: 'Descrição Tste' },
-  { id: '1', title: 'Teste Mural', description: 'Descrição Tste' },
-]
+const posts = [{ id: '1', title: 'Teste Mural', description: 'Descrição Tste' }]
 
 app.get('/all', (req, res) => {
   res.json(JSON.stringify(posts))
 })
 
-app.post('/new', (req, res) => {})
+app.post('/new', express.json(), (req, res) => {
+  let id = generateID()
+  let title = req.body.title
+  let description = req.body.description
+
+  posts.push({ id, title, description })
+  res.send('Post Adicionado')
+})
+
+function generateID() {
+  return Math.random().toString(36).substring(2, 9)
+}
 
 app.listen(PORT, () => console.log('Server running on Port: ', PORT))
