2740b9aff63afac1bb76c7fcff472e4e1bdc0746
CanezinBeto <albertocanezin@gmail.com>
2022-03-26 19:57:30 -0300

    Importanto bootstrap

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = 3000
 const path = require('path')
 const apiRoute = require('./routes/api')
 
-app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/api', apiRoute)
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.listen(PORT, () => console.log('Server running on Port: ', PORT))
